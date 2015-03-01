97095f6eb8140c7c2942b98d2784006c6dbe9943
xlucas <xavier.lucas.2@gmail.com>
2015-03-01 04:12:26 +0100

    script, readme and .vimrc added

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # go-vim-install
 Easy environment setup for Vim addicts and go developers.
+
+## Usage
+
+This little installer for ubuntu make the life simpler for go development with vim.
+The idea here is to provide 3 commands to either :
+- Download and install go from a tarball and set environment variables
+- Setup a fully working Vim environment with amazing plugins
+- Setup your go workspace with the great gocov bundled
+
+Examples :
+
+`./install.sh -go https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz`
+`./install.sh -vim`
+`./install.sh -work ~/Workspace/go`
+
+## Vim environment
+
+The script will install pathogen and the following plugins :
+
+- [vim-go](https://github.com/fatih/vim-gohttps://github.com/fatih/vim-go)
+- [YouCompleteMe](https://github.com/Valloric/YouCompleteMe)
+- [tagbar](https://github.com/majutsushi/tagbar)
+
+These two colorschemes will also be available :
+
+- [molokai](https://github.com/fatih/molokai)
+- [monokai](https://github.com/sickill/vim-monokai)
---
+++ install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+install_go() {
+
+    wget -q -O - /tmp/golang.tar.gz "$1" | sudo tar -C /usr/local -xzvf -
+
+    echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bash_profile
+
+    exit 0
+}
+
+
+install_vim() {
+
+    cwd=$(pwd)
+
+    mv ~/.vimrc ~/.vimrc.old
+
+    # Basic requirements
+    sudo apt-get install -y git vim build-essential cmake python-dev ctags
+    mkdir -p ~/.vim/{autoload,bundle,colors}
+    
+    # Pathogen
+    cd ~/.vim/autoload
+    curl -sLO "https://tpo.pe/pathogen.vim"
+
+    # Plugins
+    cd ~/.vim/bundle
+    git clone "https://github.com/majutsushi/tagbar.git"
+    git clone "https://github.com/fatih/vim-go.git"
+    git clone "https://github.com/Valloric/YouCompleteMe.git"
+    
+    cd ~/.vim/bundle/YouCompleteMe
+    git submodule update --init --recursive
+    bash install.sh
+    
+    # Colorschemes
+    cd ~/.vim/colors
+
+    curl -sLO "https://raw.githubusercontent.com/fatih/molokai/master/colors/molokai.vim"
+    curl -sLO "https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim"
+
+    cd $cwd
+
+    curl -sL -o ~/.vimrc "https://raw.githubusercontent.com/xlucas/go-vim-setup/master/.vimrc"
+    
+    exit 0
+}
+
+
+install_ws() {
+
+    cwd=$(pwd)
+
+    mkdir -p $1
+
+    echo "export GOPATH=$1"             >> ~/.bash_profile
+    echo "export PATH=\$PATH:$1/bin"    >> ~/.bash_profile
+   
+    . ~/.bash_profile
+
+    cd $GOPATH
+
+    go get github.com/axw/gocov/gocov
+    go get github.com/jstemmer/gotags
+
+    cd $cwd 
+
+    exit 0
+}
+
+
+
+case $1 in
+"-go")    install_go  $2;;
+"-vim")   install_vim $2;;
+"-work")  install_ws  $2;;
+esac
+
+echo "Usage : $0 OPTION"
+echo "      OPTION {  "
+echo "          -go TARBALL_URL : go installation"
+echo "          -vim            : vim installation"
+echo "          -work PATH      : workspace setup"
+echo "      }"
+exit 1
